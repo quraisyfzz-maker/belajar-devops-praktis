a67df44f8e2ccad63ebb8137355c8be580a2c996
quraisy <quraisyfzz@gmail.com>
2026-07-17 22:42:19 +0700

    fitur: inisialisasi awal folder migrations

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-﻿print("Halo! Script Python ini berhasil dieksekusi oleh robot CI!")
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+# Membuat jalur absolut yang aman untuk Windows maupun Linux
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
