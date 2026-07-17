88570904739ed94eca9e33847e00dc7c84a7e158
quraisy <quraisyfzz@gmail.com>
2026-07-18 00:05:02 +0700

    fitur: integrasi CRUD produk dan merapikan gitignore

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask,render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -12,3 +12,31 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+class Produk(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nama= db.Column(db.String(100), nullable=False)
+    harga= db.Column(db.Integer, nullable=False)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nama_produk=request.form['nama']
+        harga_produk=request.form['harga']
+
+        baru=Produk(nama=nama_produk, harga=harga_produk)
+        db.session.add(baru)
+        db.session.commit()
+        return redirect(url_for('index'))
+    semua_produk=Produk.query.all()
+    return render_template('index.html', produk=semua_produk)
+
+@app.route('/hapus/<int:id>')
+def hapus(id):
+    produk_hapus=Produk.query.get_or_404(id)
+    db.session.delete(produk_hapus)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
